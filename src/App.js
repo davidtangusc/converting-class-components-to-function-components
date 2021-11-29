ddde9bf5adf7c12c2d847a0beb82b4a2b585ae55
David Tang <dtang859@gmail.com>
2021-11-28 20:58:15 -0800

    3. Exercise - Display Pokemon Details

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Loading from "./Loading";
 export default function App() {
   const [pokemon, setPokemon] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [pokemonDetails, setPokemonDetails] = useState(null);
 
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=100&offset=200")
@@ -16,15 +17,48 @@ export default function App() {
       });
   }, []);
 
+  function fetchPokemonDetails(url) {
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((pokemonDetails) => {
+        setPokemonDetails(pokemonDetails);
+      });
+  }
+
   return isLoading ? (
     <Loading />
   ) : (
     <div className="flex">
       <ul>
         {pokemon.map((pokemon) => {
-          return <li key={pokemon.url}>{pokemon.name}</li>;
+          return (
+            <li key={pokemon.url}>
+              {pokemon.name}
+
+              <button
+                type="button"
+                onClick={() => {
+                  fetchPokemonDetails(pokemon.url);
+                }}
+              >
+                View
+              </button>
+            </li>
+          );
         })}
       </ul>
+      {pokemonDetails && (
+        <div>
+          <p>Moves for {pokemonDetails.name}</p>
+          <ul>
+            {pokemonDetails.moves.map((move) => {
+              return <li key={move.move.url}>{move.move.name}</li>;
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
